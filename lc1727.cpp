1c2b575d993ff2979506d96c78940c3ceeccaf54
Devvart1 <devvartkumarkumar@gmail.com>
2023-11-26 17:36:47 +0530

    lc1727 added

--- lc1727.cpp
+++ lc1727.cpp
@@ -9,8 +9,16 @@ public:
             for (int j = 0; j < m; ++j) {
                 if (i > 0 and matrix[i][j])
                     matrix[i][j] = +matrix[i - 1][j];
+                vector<int> height = matrix[i];
 
+                sort(height.begin(), height.end());
+                for (int k = 0; k < j; ++k) {
+                    int base = k + 1;
+                    int h = height[k];
+                    maxArea = max(maxArea, base * h);
+                }
             }
         }
+        return maxArea;
     }
 };
