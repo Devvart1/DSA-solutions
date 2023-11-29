03011f6c7a38db1555215dbe3dcf29e0e89e2ea8
Devvart1 <devvartkumarkumar@gmail.com>
2023-11-29 10:04:42 +0530

    lc 9(rev approch) added

--- lc9.cpp
+++ lc9.cpp
@@ -2,14 +2,25 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if (x < 0)return false;
-        string s = to_string(x);
-        int j = s.size() - 1;
-        int i = 0;
-        while (i < j) {
-            if (s[i] != s[j])return false;
-            i++;
-            j--;
+        // string s=to_string(x);
+        // int j=s.size()-1;
+        // int i=0;
+        // while(i<j){
+        //     if(s[i]!=s[j])return false;
+        //     i++;
+        //     j--;
+        // }
+        // return true;
+        long long int rev = 0;
+        int temp = x;
+        while (temp) {
+            //  cout<<"x="<<x<<endl;
+            rev = rev * 10 + temp % 10;
+            temp /= 10;
         }
-        return true;
+        // cout<<x<<endl;
+        // cout<<rev<<endl;
+        // cout<<(rev==x);
+        return rev == x;
     }
 };
